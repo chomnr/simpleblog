e99d43ec58605925dffe09b773338715d3de36a2
okjlez <zeljkovranjes@outlook.com>
2023-05-01 18:07:59 -0400

    added old theme.

--- Application/Startup.cs
+++ Application/Startup.cs
@@ -1,6 +1,34 @@
-﻿namespace Application;
+﻿using System.Reflection;
+using Application.Common.Interface;
+using Application.Infrastructure.Persistence;
+using Application.Infrastructure.Services;
+using MediatR;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Application;
 
 public class Startup
 {
+    public Startup(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
     
+    private IConfiguration _configuration { get; set; }
+    public IServiceCollection ConfigureServices(IServiceCollection services)
+    {
+        services.AddMediatR(cfg => 
+            cfg.RegisterServicesFromAssembly(Assembly.GetExecutingAssembly()));
+        return services;
+    }
+
+    public IServiceCollection AddInfrastructure(IServiceCollection services)
+    {
+        services.AddDbContext<DatabaseDbContext>(
+            options => options.UseNpgsql(_configuration.GetConnectionString("Postgres")));
+        services.AddScoped<IDomainEventService, DomainEventService>();
+        return services;
+    }
 }
