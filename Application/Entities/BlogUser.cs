3d61aa0ee4a0eefe8d0c4da0d6ecb9a5f18787ce
okjlez <zeljkovranjes@outlook.com>
2023-05-02 00:17:26 -0400

    Fixed NotAllowed Error.

--- Application/Entities/BlogUser.cs
+++ Application/Entities/BlogUser.cs
@@ -1,8 +1,33 @@
-﻿using Microsoft.AspNetCore.Identity;
+﻿using Application.Common;
+using Microsoft.AspNetCore.Identity;
 
 namespace Application.Entities;
 
 public class BlogUser : IdentityUser
 {
+    private bool _done;
+    public bool Done
+    {
+        get => _done;
+        set
+        {
+            if (value && _done == false)
+            {
+                DomainEvents.Add(new BlogUserCompletedEvent(this));
+            }
+
+            _done = value;
+        }
+    }
+    public List<DomainEvent> DomainEvents { get; } = new List<DomainEvent>();
+}
+
+public class BlogUserCompletedEvent : DomainEvent
+{
+    public BlogUserCompletedEvent(BlogUser account)
+    {
+        Account = account;
+    }
     
+    public BlogUser Account { get; }
 }
--- Application/Infrastructure/Persistence/DatabaseDbContext.cs
+++ Application/Infrastructure/Persistence/DatabaseDbContext.cs
@@ -1,4 +1,6 @@
-﻿using Application.Entities;
+﻿using Application.Common;
+using Application.Entities;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
@@ -31,4 +33,71 @@ public class DatabaseDbContext : IdentityDbContext<BlogUser>
     }
 
     public DbSet<BlogUser> BlogUsers => Set<BlogUser>();
+
+    public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default(CancellationToken))
+    {
+        var result = await base.SaveChangesAsync(cancellationToken);
+        return result;
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        builder.Ignore<DomainEvent>();
+        builder.Entity<BlogUser>(e =>
+        {
+            //do stuff here.,.. configure default account, and limit characterts and such...
+        });
+        builder.Entity<IdentityRole>(e =>
+        {
+            e.HasKey(r => r.Id);
+            e.HasIndex(r => r.NormalizedName).HasDatabaseName("RoleNameIndex").IsUnique();
+            
+            // Default Roles
+            e.HasData(
+                new IdentityRole { Id = "1", Name = "Admin", NormalizedName = "ADMIN"},
+                new IdentityRole { Id = "2", Name = "Member", NormalizedName = "MEMBER"}
+            );
+
+            e.Property(r => r.ConcurrencyStamp).IsConcurrencyToken();
+            
+            e.Property(u => u.Name).HasMaxLength(256);
+            e.Property(u => u.NormalizedName).HasMaxLength(256);
+            
+            e.HasMany<IdentityUserRole<string>>().WithOne().HasForeignKey(ur => ur.RoleId).IsRequired();
+
+            e.HasMany<IdentityRoleClaim<string>>().WithOne().HasForeignKey(rc => rc.RoleId).IsRequired();
+        });
+
+        builder.Entity<IdentityUserRole<string>>(e =>
+        {
+            e.HasKey(r => new { r.UserId, r.RoleId });
+            e.HasData(new IdentityUserRole<string>
+            {
+                RoleId = "1",
+                UserId = "1",
+            });
+        });
+        
+        builder.Entity<IdentityUserClaim<string>>(e =>
+        {
+            e.HasKey(uc => uc.Id);
+        });
+        
+        builder.Entity<IdentityUserLogin<string>>(e =>
+        {
+            e.HasKey(l => new { l.LoginProvider, l.ProviderKey });
+        });
+        
+        builder.Entity<IdentityRoleClaim<string>>(e =>
+        {
+            e.HasKey(r => r.Id);
+        });
+        
+        builder.Entity<IdentityUserToken<string>>(e =>
+        {
+            e.HasKey(t => new { t.UserId, t.LoginProvider, t.Name });
+        });
+    }
 }
