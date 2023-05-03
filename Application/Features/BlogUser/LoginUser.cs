477fb9fe531a3713f669d8cd45034adb38344ca3
okjlez <zeljkovranjes@outlook.com>
2023-05-03 15:56:07 -0400

    designed the login page.

--- Application/Features/BlogUser/LoginUser.cs
+++ Application/Features/BlogUser/LoginUser.cs
@@ -30,6 +30,8 @@ public class LoginCommand : IRequest<string>
     [Required]
     [DataType(DataType.Password)]
     public string Password { get; set; }
+
+    public bool RememberMe { get; set; } = false;
 }
 
 internal sealed class LoginAccountCommandHandler : IRequestHandler<LoginCommand, string>
@@ -43,12 +45,10 @@ internal sealed class LoginAccountCommandHandler : IRequestHandler<LoginCommand,
     
     public async Task<string> Handle(LoginCommand payLoad, CancellationToken cancellationToken)
     {
-
-        //check for whether it is email or username...
-        //var payLoadAccount = new Entities.BlogUser { UserName = payLoad.Login };
+        
         var payLoadResult =  await _signInManager.PasswordSignInAsync(payLoad.Login, 
             payLoad.Password, 
-            false,
+            payLoad.RememberMe,
             false);
         return payLoadResult.ToString();
     }
