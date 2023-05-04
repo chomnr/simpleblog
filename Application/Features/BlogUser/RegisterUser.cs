c0c28bea422bb7cec7113d238b3e42f5289408c9
okjlez <zeljkovranjes@outlook.com>
2023-05-03 20:34:54 -0400

    Added register command & web page.

---
+++ Application/Features/BlogUser/RegisterUser.cs
@@ -0,0 +1,89 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Common;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Features.BlogUser;
+
+public class RegisterUser
+{
+    private readonly IMediator _mediator;
+
+    public RegisterUser(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult<string>> Register(RegisterCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
+
+public class RegisterCommand : IRequest<string>
+{   
+    [Required]
+    public string Username { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [Required]
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
+
+internal sealed class RegisterAccountCommandHandler : IRequestHandler<RegisterCommand, string>
+{
+    private readonly UserManager<Entities.BlogUser> _userManager;
+    
+    public RegisterAccountCommandHandler(UserManager<Entities.BlogUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<string> Handle(RegisterCommand payLoad, CancellationToken cancellationToken)
+    {
+        if (!Utilities.isValidEmail(payLoad.Email, true))
+        {
+            // Probably redundant because of the [EmailAddress] annotation on Email Field.
+            return "The email is invalid."; 
+        }
+
+        if (payLoad.Password != payLoad.ConfirmPassword)
+        {
+            return "The passwords do not match.";
+        }
+        
+        var new_user = new Entities.BlogUser
+        {
+            UserName = payLoad.Username,
+            Email = payLoad.Email
+        };
+        
+        var payLoadResult = await _userManager.CreateAsync(new_user, payLoad.Password);
+        if (payLoadResult.Succeeded)
+        {
+            return "Account successfully created.";
+        }
+        else
+        {
+            return payLoadResult.Errors.ToString() ?? "Something went wrong.";
+        }
+    }
+    public class RegisterUserEvent : DomainEvent
+    {
+        public RegisterUserEvent(Entities.BlogUser account)
+        {
+            Account = account;
+        }
+
+        public Entities.BlogUser Account { get; }
+    }
+    
+}
