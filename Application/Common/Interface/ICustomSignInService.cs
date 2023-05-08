a35f42a9a8c97a7d9a7b8841f4ed4990c875f870
okjlez <zeljkovranjes@outlook.com>
2023-05-07 21:45:46 -0400

    Created LoginWithEmailOrUsername method.

---
+++ Application/Common/Interface/ICustomSignInService.cs
@@ -0,0 +1,9 @@
+﻿using Application.Features.BlogUser;
+using Microsoft.AspNetCore.Identity;
+
+namespace Application.Common.Interface;
+
+public interface ICustomSignInService
+{
+    Task<SignInResult> LoginWithEmailOrUsername(LoginCommand command);
+}
