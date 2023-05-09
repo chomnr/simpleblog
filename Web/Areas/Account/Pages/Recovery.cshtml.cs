d8ca7d6c11979cdc6db975fe6a2774e8f0a6b4aa
okjlez <zeljkovranjes@outlook.com>
2023-05-08 20:21:46 -0400

    added asp-validation to form & post functioanlity.. unfinished.

--- Web/Areas/Account/Pages/Recovery.cshtml.cs
+++ Web/Areas/Account/Pages/Recovery.cshtml.cs
@@ -1,11 +1,63 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Diagnostics.CodeAnalysis;
+using Application.Features.BlogUser;
+using MediatR;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using SendGrid;
 
 namespace Web.Areas.Account.Pages;
 
-public class Recovery : PageModel
+public abstract class MediatorRecoveryModel : PageModel
+{   
+    [BindProperty]
+    public RecoveryCommand Input { get; set; } = default!;
+    
+    public string? ReturnUrl { get; set; }
+    
+    [TempData]
+    public string? ErrorMessage { get; set; }
+    
+    public virtual Task OnGetAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null) => throw new NotImplementedException();
+    
+    public virtual Task<IActionResult> OnPostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null) => throw new NotImplementedException();
+}
+
+internal sealed class MediatorRecoveryModel<TUser> : MediatorRecoveryModel where TUser : class
 {
-    public void OnGet()
+    private readonly IMediator _mediator;
+
+    public MediatorRecoveryModel(IMediator mediator)
     {
+        _mediator = mediator;
+    }
+    
+    // todo fix.
+    public override async Task OnGetAsync(string? returnUrl = null)
+    {
+        if (!string.IsNullOrEmpty(ErrorMessage))
+        {
+            ModelState.AddModelError(string.Empty, ErrorMessage);
+        }
+
+        returnUrl ??= Url.Content("~/");
+
+        await HttpContext.SignOutAsync();
         
+        ReturnUrl = returnUrl;
+    }
+    
+    public override async Task<IActionResult> OnPostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null)
+    {
+        if (ModelState.IsValid)
+        {
+            var result = await _mediator.Send(Input);
+            if (result.Succeeded)
+            {
+               // reset
+            }
+            // we dont want them to know if the email exists or not so send success message regardless ok...
+        }
+        return Page();
     }
 }
