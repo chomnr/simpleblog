d6897e0144e67fa5b4f0a1d8dfb99b83ff7166cd
okjlez <zeljkovranjes@outlook.com>
2023-05-08 20:22:28 -0400

    added resetpassword form unfinished.

---
+++ Web/Areas/Account/Pages/Recovery/ResetPassword.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Areas.Account.Pages.Recovery;
+
+public class ResetPassword : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
