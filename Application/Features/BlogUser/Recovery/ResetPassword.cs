600dad98374c15644230b367f1dfe27612fc31d1
okjlez <zeljkovranjes@outlook.com>
2023-05-08 21:33:49 -0400

    added foundation for ResetPassword

---
+++ Application/Features/BlogUser/Recovery/ResetPassword.cs
@@ -0,0 +1,58 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Common;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Features.BlogUser.Recovery;
+
+public class ResetPassword : FeatureController
+{
+    private readonly IMediator _mediator;
+
+    public ResetPassword(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult<IdentityResult>> Login(PasswordResetCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
+
+public class PasswordResetCommand : IRequest<IdentityResult>
+{
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+    [DataType(DataType.Password)]
+    public string ConfirmPassword { get; set; }
+}
+
+public class PasswordResetCommandHandler : IRequestHandler<PasswordResetCommand, IdentityResult>
+{
+    private readonly UserManager<Entities.BlogUser> _userManager;
+
+    public PasswordResetCommandHandler(UserManager<Entities.BlogUser> userManager)
+    {
+        _userManager = userManager;
+    }
+
+    public async Task<IdentityResult> Handle(PasswordResetCommand payLoad, CancellationToken cancellationToken)
+    {
+       return IdentityResult.Success;
+    }
+    
+    public class PasswordResetEvent : DomainEvent
+    {
+        public PasswordResetEvent(Entities.BlogUser user)
+        {
+            User = user;
+        }
+
+        public Entities.BlogUser User { get; }
+    }
+}
