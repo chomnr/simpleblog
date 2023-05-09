312e53c21ce7c1454150b20e61ced1e9fa5c5e28
okjlez <zeljkovranjes@outlook.com>
2023-05-08 20:22:02 -0400

    added RecoveryUser.cs page. universal.

---
+++ Application/Features/BlogUser/RecoveryUser.cs
@@ -0,0 +1,86 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Common;
+using Application.Common.Interface;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Features.BlogUser;
+
+public class RecoveryUser : FeatureController
+{
+    private readonly IMediator _mediator;
+
+    public RecoveryUser(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult<IdentityResult>> Login(RecoveryCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
+
+public class RecoveryCommand : IRequest<IdentityResult>
+{   
+    [Required]
+    public string Email { get; set; }
+}
+
+internal sealed class RecoveryUserCommandHandler : IRequestHandler<RecoveryCommand, IdentityResult>
+{
+    private readonly UserManager<Entities.BlogUser> _userManager;
+    private readonly IWebHelperService _webHelper;
+    private readonly IEmailSenderService _emailSender;
+    private readonly IConfiguration _configuration;
+    
+    public RecoveryUserCommandHandler(UserManager<Entities.BlogUser> userManager, 
+        IWebHelperService webHelper,
+        IEmailSenderService emailSender,
+        IConfiguration configuration)
+    {
+        _userManager = userManager;
+        _webHelper = webHelper;
+        _emailSender = emailSender;
+        _configuration = configuration;
+    }
+    
+    public async Task<IdentityResult> Handle(RecoveryCommand payLoad, CancellationToken cancellationToken)
+    {
+        var error = new CustomError();
+        var config = _configuration.GetSection("Authentication").GetSection("Email");
+        var email = payLoad.Email; 
+        
+        if (!Utilities.IsValidEmail(email, false))
+        {
+            return IdentityResult.Failed(error.InvalidEmail());
+        }
+
+        var user = await _userManager.FindByEmailAsync(payLoad.Email);
+        if (user != null)
+        {
+            var token = await _userManager.GeneratePasswordResetTokenAsync(user);
+            var basePath = _webHelper.GetBaseUrl();
+            var confirmPath = $"/account/recovery/resetpassword" +
+                              $"?userId={Uri.EscapeDataString(user.Id)}" +
+                              $"&resetToken={Uri.EscapeDataString(token)}";
+            var formatHtml = $"<a href={basePath + confirmPath}>Reset Password</a>";
+            await _emailSender.SendEmailAsync(payLoad.Email, config["EmailResetPasswordSubject"], formatHtml);
+        }
+        return IdentityResult.Failed(error.DefaultError());
+    }
+    
+    public class RecoveryUserEvent : DomainEvent
+    {
+        public RecoveryUserEvent(Entities.BlogUser user)
+        {
+            User = user;
+        }
+
+        public Entities.BlogUser User { get; }
+    }
+}
