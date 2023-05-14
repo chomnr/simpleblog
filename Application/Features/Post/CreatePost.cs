8cb1d9efa731ee0e1e8521cd3f09b1d932a10efe
okjlez <zeljkovranjes@outlook.com>
2023-05-14 00:38:43 -0400

    added CreatePost feature.

---
+++ Application/Features/Post/CreatePost.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+using Application.Common;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Application.Features.Post;
+
+public class CreatePost : FeatureController
+{
+    private readonly IMediator _mediator;
+
+    public CreatePost(IMediator mediator)
+    {
+        _mediator = mediator;
+    }
+    
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public async Task<ActionResult<string>> Login(CreatePostCommand command)
+    {
+        return await _mediator.Send(command);
+    }
+}
+
+public class CreatePostCommand : IRequest<string>
+{   
+    [Required]
+    public string Title { get; set; }
+    [Required]
+    public string Body { get; set; }
+    [Required]
+    public string Category { get; set; }
+    [Required]
+    public List<string> Tags { get; set; }
+}
