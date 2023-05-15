e989c0d272efa3b794aa818360fdb5c78b0d47c3
okjlez <zeljkovranjes@outlook.com>
2023-05-15 02:15:40 -0400

    foundation for post create

--- Web/Areas/Post/Pages/Create.cshtml.cs
+++ Web/Areas/Post/Pages/Create.cshtml.cs
@@ -1,11 +1,53 @@
-﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+﻿using System.Diagnostics.CodeAnalysis;
+using Application.Features.Post;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace Web.Areas.Post.Pages;
 
-public class Create : PageModel
+public class MediatorCreatePostModel : PageModel
 {
-    public void OnGet()
+    [BindProperty]
+    public CreatePostCommand Input { get; set; } = default!;
+    
+    public string? ReturnUrl { get; set; }
+    
+    [TempData]
+    public string? ErrorMessage { get; set; }
+    
+    public virtual Task OnGetAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null) => throw new NotImplementedException();
+    
+    public virtual Task<IActionResult> OnPostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null) => throw new NotImplementedException();
+}
+
+internal sealed class MediatorCreatePostModel<TPost> : MediatorCreatePostModel where TPost : class
+{
+    private readonly IMediator _mediator;
+
+    public MediatorCreatePostModel(IMediator mediator)
     {
+        _mediator = mediator;
+    }
+    
+    public override async Task OnGetAsync(string? returnUrl = null)
+    {
+        if (!string.IsNullOrEmpty(ErrorMessage))
+        {
+            ModelState.AddModelError(string.Empty, ErrorMessage);
+        }
+
+        returnUrl ??= Url.Content("~/");
         
+        ReturnUrl = returnUrl;
+    }
+    
+    public override async Task<IActionResult> OnPostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null)
+    {
+        if (ModelState.IsValid)
+        {
+           
+        }
+        return Page();
     }
 }
