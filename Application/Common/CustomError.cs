7da04d8d6b29afa7b43f95b650996af758895c30
okjlez <zeljkovranjes@outlook.com>
2023-05-16 22:30:38 -0400

    added InputLengthTooSmall & InputLengthTooLong

--- Application/Common/CustomError.cs
+++ Application/Common/CustomError.cs
@@ -22,12 +22,30 @@ public class CustomError : IdentityErrorDescriber
         };
     }
     
-    public IdentityError BadNameConstraints()
+    public IdentityError InvalidName()
     {
         return new IdentityError
         {
             Code = "WrongConstraints",
             Description = "Either the first or last name do not follow the constraints."
         };
+    }
+
+    public IdentityError InputLengthTooSmall(string input, int minLength)
+    {
+        return new IdentityError
+        {
+            Code = "InputTooSmall",
+            Description = input + " is too small the minimum length must be " + minLength
+        }; 
+    }
+    
+    public IdentityError InputLengthTooLong(string input, int maxLength)
+    {
+        return new IdentityError
+        {
+            Code = "InputTooSmall",
+            Description = input + " is too long the maximum length must be " + maxLength
+        }; 
     }
 }
