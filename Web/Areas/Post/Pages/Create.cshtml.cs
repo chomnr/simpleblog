058aa23df50676ee05ffcad3bc2d20c87103e295
okjlez <zeljkovranjes@outlook.com>
2023-05-16 22:29:33 -0400

    fixed tags issue.

--- Web/Areas/Post/Pages/Create.cshtml.cs
+++ Web/Areas/Post/Pages/Create.cshtml.cs
@@ -1,8 +1,15 @@
 ﻿using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using Application.Entities;
 using Application.Features.Post;
+using EllipticCurve.Utils;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Newtonsoft.Json;
 
 namespace Web.Areas.Post.Pages;
 
@@ -11,6 +18,9 @@ public class MediatorCreatePostModel : PageModel
     [BindProperty]
     public CreatePostCommand Input { get; set; } = default!;
     
+    [BindProperty]
+    public string TagHolder { get; set; }
+
     public string? ReturnUrl { get; set; }
     
     [TempData]
@@ -24,29 +34,46 @@ public class MediatorCreatePostModel : PageModel
 internal sealed class MediatorCreatePostModel<TPost> : MediatorCreatePostModel where TPost : class
 {
     private readonly IMediator _mediator;
+    private readonly SignInManager<BlogUser> _signInManager;
+    private readonly IHttpContextAccessor _httpContextAccessor;
 
-    public MediatorCreatePostModel(IMediator mediator)
+    public MediatorCreatePostModel(IMediator mediator, SignInManager<BlogUser> signInManager, IHttpContextAccessor httpContextAccessor)
     {
         _mediator = mediator;
+        _signInManager = signInManager;
+        _httpContextAccessor = httpContextAccessor;
     }
     
     public override async Task OnGetAsync(string? returnUrl = null)
-    {
+    {   
         if (!string.IsNullOrEmpty(ErrorMessage))
         {
             ModelState.AddModelError(string.Empty, ErrorMessage);
         }
 
-        returnUrl ??= Url.Content("~/");
-        
+        if (!_signInManager.IsSignedIn(_httpContextAccessor.HttpContext.User))
+        {
+            Response.Redirect("/account/login");
+        }
+        else
+        {
+            returnUrl ??= Url.Content("~/");
+        }
+
         ReturnUrl = returnUrl;
     }
     
     public override async Task<IActionResult> OnPostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null)
     {
         if (ModelState.IsValid)
-        {
-           
+        {   
+            // Converts {"[\"yes\",\"hello\"]"} to {dog,yes,hello,readable} in Database.
+            Input.Body = Base64.encode(Encoding.ASCII.GetBytes(Input.Body));
+            if (TagHolder != null) {
+                Input.Tags = JsonConvert.DeserializeObject<List<string>>(TagHolder);
+            }
+            var result = await _mediator.Send(Input);
+            Console.WriteLine(result);
         }
         return Page();
     }
