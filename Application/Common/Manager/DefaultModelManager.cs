3e6454427705bb6647bc6c2d5b8082345ee36e4e
okjlez <zeljkovranjes@outlook.com>
2023-05-16 22:28:51 -0400

    added modelmanager helper class.

---
+++ Application/Common/Manager/DefaultModelManager.cs
@@ -0,0 +1,34 @@
+﻿using Application.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Common.Manager;
+
+public class DefaultModelManager
+{
+    private readonly IConfiguration _configuration;
+
+    public DefaultModelManager(IConfiguration configuration)
+    {
+        _configuration = configuration;
+    }
+
+    public (BlogUser, IdentityUserRole<string>) CreateDefaultModelAdminAccount(ModelBuilder builder)
+    {
+        var admin = _configuration.GetSection("Authorization").GetSection("Admin");
+        var defaultAdminUser = BlogUserHelper.CreateBlogUser(
+            admin["FirstName"], 
+            admin["LastName"], 
+            admin["Username"], 
+            admin["Password"], 
+            admin["Email"]);
+        var adminRole = new IdentityUserRole<string>
+        {
+            UserId = defaultAdminUser.Id,
+            RoleId = "1"
+        };
+        return (defaultAdminUser, adminRole);
+    }
+}
---
+++ Application/Infrastructure/Persistence/DatabaseDbModel.cs
@@ -0,0 +1,51 @@
+﻿using Application.Common.Manager;
+using Application.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+
+namespace Application.Infrastructure.Persistence;
+
+public static class DatabaseDbModel
+{
+    public static void AutoRenameIdentityTables(ModelBuilder builder)
+    {
+        builder.Entity<BlogUser>(bu => { bu.ToTable("users"); });
+        builder.Entity<Post>(pt => { pt.ToTable("posts"); });
+        
+        builder.Entity<IdentityRole>(bu => { bu.ToTable("roles"); });
+        builder.Entity<IdentityRoleClaim<string>>(bu => { bu.ToTable("role_claims"); });
+
+        builder.Entity<IdentityUserRole<string>>(bu => { bu.ToTable("user_roles"); });
+        builder.Entity<IdentityUserClaim<string>>(bu => { bu.ToTable("user_claims"); });
+        builder.Entity<IdentityUserLogin<string>>(bu => { bu.ToTable("user_logins"); });
+        builder.Entity<IdentityUserToken<string>>(bu => { bu.ToTable("user_tokens"); });
+    }
+    public static void BaseUserModel(ModelBuilder builder, IConfiguration configuration)
+    {
+        var manager = new DefaultModelManager(configuration);
+        var (adminUser, adminRole) =  manager.CreateDefaultModelAdminAccount(builder);
+        
+        builder.Entity<BlogUser>(e =>
+        {
+            e.Property(u => u.UserName).HasMaxLength(255);
+            e.Property(u => u.Email).HasMaxLength(256);
+            e.Property(u => u.ConcurrencyStamp).IsConcurrencyToken();
+
+            e.HasData(adminUser);
+            
+            /*
+            e.HasMany<IdentityUserClaim<string>>().WithOne().HasForeignKey(uc => uc.UserId).IsRequired();
+            e.HasMany<IdentityUserLogin<string>>().WithOne().HasForeignKey(ul => ul.UserId).IsRequired();
+            e.HasMany<IdentityUserToken<string>>().WithOne().HasForeignKey(ut => ut.UserId).IsRequired();
+            e.HasMany<IdentityUserRole<string>>().WithOne().HasForeignKey(ur => ur.UserId).IsRequired();
+            e.HasMany<Post>().WithOne().HasForeignKey(p => p.UserId).IsRequired();
+            */
+        });
+
+        builder.Entity<IdentityUserRole<string>>(e =>
+        {
+            e.HasData(adminRole);
+        });
+    }
+}
