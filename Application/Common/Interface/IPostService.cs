47965bc68356af4400c05b4afa1cfcfbc21faddf
okjlez <zeljkovranjes@outlook.com>
2023-05-17 23:46:17 -0400

    added RetrieveAsync

--- Application/Common/Interface/IPostService.cs
+++ Application/Common/Interface/IPostService.cs
@@ -1,8 +1,10 @@
 ﻿using Application.Features.Post;
+using Microsoft.AspNetCore.Mvc;
 
 namespace Application.Common.Interface;
 
 public interface IPostService
 {
     Task<bool> CreateAsync(CreatePostCommand command, string userId);
+    Task<JsonResult> RetrieveAsync(RetrievePostsCommand command, bool partial);
 }
