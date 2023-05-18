64655e0514734c11132a52708e6928b43feed6f3
okjlez <zeljkovranjes@outlook.com>
2023-05-17 23:47:02 -0400

    added RetrieveAsync for posts.

--- Application/Infrastructure/Services/PostService.cs
+++ Application/Infrastructure/Services/PostService.cs
@@ -3,6 +3,9 @@ using Application.Common.Interface;
 using Application.Entities;
 using Application.Features.Post;
 using Application.Infrastructure.Persistence;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 namespace Application.Infrastructure.Services;
 
@@ -53,10 +56,46 @@ public class PostService : IPostService
             NormalizedTitle = title.ToUpper(),
             Body = body,
             Tags = tags,
-            Done = true
+            Done = false
         };
         posts.Add(post);
         await _context.SaveChangesAsync();
         return true;
+    }
+
+    public async Task<JsonResult> RetrieveAsync(RetrievePostsCommand command, bool partial)
+    {
+        var page = command.Page;
+        var retrieval = page == 1 ? 7 : 12;
+        
+        var posts = _context.Posts
+            .Join(
+                _context.Users,
+                post => post.UserId,
+                user => user.Id,
+                (post, user) => new { Post = post, UserName = user.UserName }
+            )
+            .Select(postWithUserName => new
+            {
+                postWithUserName.Post.Id,
+                postWithUserName.Post.UserId,
+                postWithUserName.Post.Title,
+                postWithUserName.Post.Body,
+                postWithUserName.Post.DateCreated,
+                postWithUserName.Post.Tags,
+                UserName = postWithUserName.UserName,
+            });
+        
+        var result = await posts.Take(retrieval).ToListAsync();
+        
+        if (page != 1)
+        {
+            retrieval = page == 2 ? 7 : 12;
+            result = await posts.Skip(page - 1 * retrieval).Take(retrieval).ToListAsync();
+        }
+        
+        var json = JsonConvert.SerializeObject(result);
+
+        return new JsonResult(json);
     }
 }
