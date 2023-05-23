08889438f770f1c5834c3febaf9143cf4d57db9f
okjlez <zeljkovranjes@outlook.com>
2023-05-22 23:12:08 -0400

    adjustments to create post. working with updated ui.

---
+++ Web/Areas/Post/Pages/Create/Confirmation.cshtml.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Areas.Post.Pages.Create;
+
+public class Confirmation : PageModel
+{
+    public void OnGet()
+    {
+        
+    }
+}
