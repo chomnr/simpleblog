9aa577134707c0ed6fe496eadd7d5b9fce6b84b7
okjlez <zeljkovranjes@outlook.com>
2023-05-27 10:37:03 -0400

    added tests project.

---
+++ Tests/IntegrationTests/TestBase.cs
@@ -0,0 +1,5 @@
+﻿namespace Tests.IntegrationTests;
+
+public class TestBase
+{
+}
---
+++ Tests/IntegrationTests/Testing.cs
@@ -0,0 +1,42 @@
+﻿using Application;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Moq;
+
+namespace Tests.IntegrationTests;
+
+[SetUpFixture]
+public class Testing
+{
+    private static IConfigurationRoot t_configuration = null!;
+    private static string? t_currentUserId;
+    
+    [OneTimeSetUp]
+    public void RunThisFirstBeforeAnything()
+    {
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(Directory.GetCurrentDirectory())
+            .AddJsonFile("appsettings.json", true, true)
+            .AddEnvironmentVariables();
+        t_configuration = builder.Build();
+
+        var startup = new Startup(t_configuration);
+
+        var services = new ServiceCollection();
+        
+        services.AddSingleton(Mock.Of<IWebHostEnvironment>(w =>
+            w.EnvironmentName == "Development" &&
+            w.ApplicationName == "SimpleBlog.Web"));
+        
+        services.AddLogging();
+
+        startup.ConfigureServices(services);
+        
+    }
+    
+    [OneTimeTearDown]
+    public void RunAfterAnyTests()
+    {
+    }
+}
