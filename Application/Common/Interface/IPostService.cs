7e32b65484233e193c01f2dc9914fd720245bcdc
okjlez <zeljkovranjes@outlook.com>
2023-05-27 10:35:22 -0400

    added spacing to be consistent with style.

--- Application/Common/Interface/IPostService.cs
+++ Application/Common/Interface/IPostService.cs
@@ -8,11 +8,17 @@ namespace Application.Common.Interface;
 public interface IPostService
 {
     Task<bool> CreateAsync(CreatePostCommand command, string userId);
+    
     Task<JsonResult> RetrieveAllAsync(RetrievePostsCommand command, bool partial);
     
     Task<JsonResult> RetrieveSpecificAsync(RetrievePostCommand command);
+    
     Task<JsonResult> RetrieveAllFromUserAsync(ViewUserCommand command);
     
+    
     Task<JsonResult> RetrieveAllByTag(RetrievePostsByTagCommand command);
+    
     Task<bool> DeleteAsync(DeletePostCommand command, string userId, string role);
+    
+    Task<bool> EditAsync(EditPostCommand command);
 }
