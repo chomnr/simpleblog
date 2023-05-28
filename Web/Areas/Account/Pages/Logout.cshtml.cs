d269f47b53744668366fbd7f27100c0fd0454b96
okjlez <zeljkovranjes@outlook.com>
2023-05-28 15:08:31 -0400

    added logout.

---
+++ Web/Areas/Account/Pages/Logout.cshtml.cs
@@ -0,0 +1,48 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using Application.Entities;
+using MediatR;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Web.Areas.Account.Pages;
+
+public class LogoutModel : PageModel
+{
+    public virtual Task OnGetAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null) => throw new NotImplementedException();
+
+    public virtual Task<IActionResult>
+        OnPostAsync([StringSyntax(StringSyntaxAttribute.Uri)] string? returnUrl = null) =>
+        throw new NotImplementedException();
+    
+}
+
+internal sealed class LogoutModel<TUser> : MediatorLoginModel where TUser : class
+{
+    private readonly SignInManager<BlogUser> _signInManager;
+
+    public LogoutModel(SignInManager<BlogUser> signInManager)
+    {
+        _signInManager = signInManager;
+    }
+    
+    // todo fix.
+    public override async Task OnGetAsync(string? returnUrl = null)
+    {
+        if (!string.IsNullOrEmpty(ErrorMessage))
+        {
+            ModelState.AddModelError(string.Empty, ErrorMessage);
+        }
+
+        returnUrl ??= Url.Content("~/");
+
+        if (!_signInManager.IsSignedIn(HttpContext.User))
+        {
+            Response.Redirect("/");
+        }
+
+        await _signInManager.SignOutAsync();
+        
+        ReturnUrl = returnUrl;
+    }
+}
