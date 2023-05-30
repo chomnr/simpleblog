2803e59eada223208d0be85270a0e4b89e6d4518
okjlez <zeljkovranjes@outlook.com>
2023-05-30 02:12:44 -0400

    added static values inside appsettings and added postgresql-client.

--- Web/build.sh
+++ Web/build.sh
@@ -1,9 +1,11 @@
 ﻿#!/bin/bash
 
+#sed -i "s#Host=localhost; Database=blog; Username=postgres; Password=postgres!| Database=$POSTGRES_DB; Username=$POSTGRES_USER; Password=$POSTGRES_PASS;|" appsettings.json
+
+psql -U postgres -c "CREATE DATABASE ${POSTGRES_DB};" + 
+
 # Run EF Migrations
 dotnet ef migrations add "InitialMigration" --project Application --startup-project Web --output-dir Infrastructure/Persistence/Migrations
 
 # Apply Database Update
 dotnet ef database update --project Application --startup-project Web
-
-sed -i "s|Host={localhost}; Database={db}; Username={username}; Password={password}!|Host=$POSTGRES_HOST; Database=$POSTGRES_DB; Username=$POSTGRES_USER; Password=$POSTGRES_PASS;|" appsettings.json
