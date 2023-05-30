1c7ccad50e630e53868c194097933f6eb07261b1
okjlez <zeljkovranjes@outlook.com>
2023-05-30 00:43:21 -0400

    added postgres and build.sh to Dockerfile

---
+++ Web/build.sh
@@ -0,0 +1,7 @@
+﻿#!/bin/bash
+
+# Run EF Migrations
+dotnet ef migrations add "InitialMigration" --project Application --startup-project Web --output-dir Infrastructure/Persistence/Migrations
+
+# Apply Database Update
+dotnet ef database update --project Application --startup-project Web
