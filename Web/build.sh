f918290717a7936ae670f2633f7747d72bcce1cd
okjlez <zeljkovranjes@outlook.com>
2023-05-30 13:11:15 -0400

    stopped trying to run multiple images. doing it the docker way.

--- Web/build.sh
+++ Web/build.sh
@@ -1,4 +1,3 @@
 ﻿#!/bin/bash
 
 dotnet ef migrations add "InitialMigration" --project ../Application --startup-project ../Web --output-dir Infrastructure/Persistence/Migrations
-dotnet ef database update --project ../Application --startup-project ../Web
