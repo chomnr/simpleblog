db0155b517464d14849f9046dcc84f373ce502c7
okjlez <zeljkovranjes@outlook.com>
2023-05-30 12:05:25 -0400

    should fix "Unable to retrieve project metadata. Ensure it's an SDK-style project."

--- Web/build.sh
+++ Web/build.sh
@@ -1,4 +1,4 @@
 ﻿#!/bin/bash
 
 dotnet ef migrations add "InitialMigration" --project ../Application --startup-project ../Web --output-dir Infrastructure/Persistence/Migrations
-dotnet ef database update --project ../Application --startup-project Web
+dotnet ef database update --project ../Application --startup-project ../Web
