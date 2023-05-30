dce8f2e2e906b42c2831c785761be8df0532a6ec
okjlez <zeljkovranjes@outlook.com>
2023-05-30 12:02:53 -0400

    use bash instead of./ to ensure #!/bin/bash is seen.

--- Web/build.sh
+++ Web/build.sh
@@ -1,5 +1,4 @@
-﻿#!/usr/bin/env bash
-##!/bin/bash
+﻿#!/bin/bash
 
 dotnet ef migrations add "InitialMigration" --project ../Application --startup-project ../Web --output-dir Infrastructure/Persistence/Migrations
 dotnet ef database update --project ../Application --startup-project Web
