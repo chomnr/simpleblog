82e3a0c7293714a89dbdf3cdde711d4bea17dd2f
okjlez <zeljkovranjes@outlook.com>
2023-05-30 02:14:51 -0400

    removed +.

--- Web/build.sh
+++ Web/build.sh
@@ -2,7 +2,7 @@
 
 #sed -i "s#Host=localhost; Database=blog; Username=postgres; Password=postgres!| Database=$POSTGRES_DB; Username=$POSTGRES_USER; Password=$POSTGRES_PASS;|" appsettings.json
 
-psql -U postgres -c "CREATE DATABASE ${POSTGRES_DB};" + 
+psql -U postgres -c "CREATE DATABASE ${POSTGRES_DB};"
 
 # Run EF Migrations
 dotnet ef migrations add "InitialMigration" --project Application --startup-project Web --output-dir Infrastructure/Persistence/Migrations
