6fb3f55eaf1f443ec9cf4c9bf44b78792a302920
okjlez <zeljkovranjes@outlook.com>
2023-05-31 18:05:08 -0400

    removing redundant stuff.

--- Web/Program.cs
+++ Web/Program.cs
@@ -54,12 +54,12 @@ if (app.Environment.IsDevelopment())
 }
 else
 {
-    app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    //app.UseHsts();
+    app.UseHttpsRedirection();
     Console.WriteLine("Running SimpleBlog in Production.");
 }
-
+// The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+//app.UseExceptionHandler("/Error");
+//app.UseHsts();
 //app.UseHttpsRedirection();
 
 app.UseStaticFiles();
