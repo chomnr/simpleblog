7f773e2f37b778edebf9afb497d3098301ee5302
okjlez <zeljkovranjes@outlook.com>
2023-05-31 18:02:35 -0400

    telling us we're running in production

--- Web/Program.cs
+++ Web/Program.cs
@@ -57,6 +57,7 @@ else
     app.UseExceptionHandler("/Error");
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     //app.UseHsts();
+    Console.WriteLine("Running SimpleBlog in Production.");
 }
 
 //app.UseHttpsRedirection();
