c396d8c17e85181a7dae37ed3e53f6b8a1de0536
okjlez <zeljkovranjes@outlook.com>
2023-06-01 01:40:36 -0400

    adjusted href on a variety of pages for it to support both Pathbase & Regular.

---
+++ Application/Common/Security/CustomAuthorize.cs
@@ -0,0 +1,40 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Application.Common.Security;
+
+public class CustomAuthorize: TypeFilterAttribute
+{
+    public CustomAuthorize() : base(typeof(CustomAuthorizationFilter))
+    {
+    }
+
+    private class CustomAuthorizationFilter : IAuthorizationFilter
+    {
+
+        public CustomAuthorizationFilter()
+        {
+            
+        }
+
+        public void OnAuthorization(AuthorizationFilterContext context)
+        {
+            // Perform your custom authorization logic here
+            // Example: Check if the user has a specific role or meets certain requirements
+            var isAuthorized = CheckCustomAuthorization(context.HttpContext.User);
+    
+            if (!isAuthorized)
+            {
+                context.HttpContext.Response.Redirect(context.HttpContext.Request.PathBase + "/");
+            } 
+        }
+
+        private bool CheckCustomAuthorization(ClaimsPrincipal user)
+        {
+            return user.Identity != null ? user.Identity.IsAuthenticated : false;
+        }
+    }
+}
